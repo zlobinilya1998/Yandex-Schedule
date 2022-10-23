d88aba2156df3fdf2bb1eabac2e442ae69025d2f
zlobin_ilya <qwertin1998@gmail.com>
2022-10-23 23:47:38 +0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { config } from 'dotenv'
+import express from 'express'
+
+
+const PORT = process.env.PORT || 3000
+config()
+const app = express()
+
+app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.post('/message', async (req, res) => {
+    res.send('Done')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
