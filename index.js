b09642049f12bcd486e39b9da8a1cd1ea50aa776
zlobin_ilya <qwertin1998@gmail.com>
2022-10-24 01:32:15 +0300

    2

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const PORT = process.env.PORT || 3000
 config()
 const app = express()
 
+
+const getEvents = async () => {
+    const today = new Date().toLocaleString().split(',')[0];
+    let tommorow = new Date()
+    tommorow.setDate(tommorow.getDate() + 1);
+    tommorow = tommorow.toLocaleString().split(',')[0]
+
+    const api_url = 'https://profsalon.org/CRM/msc_persona_malaya_nikitskaya/desktop/loadScheduleEvents';
+    const cookies = "PHPSESSID=aid0n4viav3ke9dd377946kpb0;_ym_uid=1666536337347538161;_ym_d=1666536337;_ym_isad=2"
+    const {data} = await axios.post(api_url, {
+        day: tommorow,
+    }, {
+        headers: {
+            Cookie: cookies,
+        },
+    })
+    return data;
+}
+
 app.use(express.json())
 app.use(
     express.urlencoded({
@@ -14,33 +33,10 @@ app.use(
     })
 )
 
-const cookies = "PHPSESSID=aid0n4viav3ke9dd377946kpb0;_ym_uid=1666536337347538161;_ym_d=1666536337;_ym_isad=2"
 
 app.post('/', async (req, res) => {
-    let responseText = ''
-    const { data } = await axios.post('https://profsalon.org/CRM/msc_persona_malaya_nikitskaya/desktop/loadScheduleEvents', {
-        day: "22.10.2022",
-    }, {
-        headers: {
-            Cookie: cookies,
-        },
-    })
-
-    let events = data.events.filter(event => event.id).map(event => ({
-        ...event,
-        start: new Date(event.start)
-    })).sort((a,b) => a.start - b.start)
-
-
-    events.forEach(event => {
-        responseText += ', в ' + event.start.getHours() + ' часов: ' + event.name
-    })
-
     const {version, session, request} = req.body
-
-    console.log(responseText)
-
-    const userCommand = request.command;
+    const userCommand = request.command.toLowerCase();
     const response = {
         version,
         session,
@@ -48,14 +44,34 @@ app.post('/', async (req, res) => {
             end_session: false,
         }
     }
-    events.length = 0;
 
-    if (session.new) response.response.text = 'Я умею находить записи на сегодня и на завтра, на какой день вам показать?'
-    else if (!events.length){
-        response.response.text = 'На указанный период никто не записался';
-        response.response.end_session = true;
+    let responseText = ''
+    let data = null;
+    try {
+        data = await getEvents();
+    } catch (e) {
+        response.response.text = 'Произошла ошибка при запросе данных из салона'
+        res.send(response)
     }
-    else {
+
+    let events = data.events.filter(event => event.id).map(event => ({
+        ...event,
+        start: new Date(event.start)
+    })).sort((a, b) => a.start - b.start)
+
+    events.forEach(event => {
+        responseText += ', в ' + event.start.getHours() + ' часов: ' + event.name
+    })
+
+    if (session.new) {
+        response.response.text = 'Я умею находить записи на сегодня и на завтра, на какой день вам показать?';
+        res.send(response);
+    }
+
+    if (!events.length) {
+        response.response.text = 'На указанный период никто не записался, можете отдохнуть';
+        response.response.end_session = true;
+    } else {
         response.response.text = 'Запись на завтра - это ' + responseText;
         response.response.end_session = true;
     }
