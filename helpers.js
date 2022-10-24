745d4c9755e6563985de4f8ce1ed325bfcf3becf
zlobin_ilya <qwertin1998@gmail.com>
2022-10-24 11:27:25 +0300

    2

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 export class EventsTransformer {
     static transformIntoView(events){
         return events
-            .filter(event => event.id)
+            .filter(event => event.id !== 0)
             .map(event => ({...event, start: new Date(event.start)}))
             .sort((a, b) => a.start - b.start)
     }
