3298bb12410bc1ade42c2d0b0f5722bbf39c9b6d
zlobin_ilya <qwertin1998@gmail.com>
2022-10-25 17:24:48 +0300

    Remove: root route

--- index.js
+++ index.js
@@ -11,54 +11,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
-app.post('/', async (req, res) => {
-    const {version, session, request} = req.body
-    const userCommand = request.command.toLowerCase();
-    const forceToday = userCommand.includes('сегодня');
-    const step = session.message_id;
-    const response = {
-        version,
-        session,
-        response: {
-            end_session: false,
-        }
-    }
-
-    let responseText = ''
-    let data = [];
-    try {
-        data = await SalonService.loadEvents(forceToday)
-    } catch (e) {
-        response.response.text = 'Произошла ошибка при запросе данных из салона'
-        response.response.end_session = true;
-        return res.send(response);
-    }
-
-    const events = EventsTransformer.transformIntoView(data.events);
-    events.forEach(event => responseText += EventsTransformer.getEventText(event));
-
-    if (session.new) {
-        response.response.text = 'Я умею находить записи на сегодня и на завтра, на какой день вам показать?';
-        return res.send(response);
-    }
-
-    //Only for first step
-    if (step === 1) {
-        if (events.length) {
-            response.response.text = `Запись на ${forceToday ? 'сегодня' : 'завтра'}:` + responseText;
-            response.response.end_session = true;
-        } else {
-            response.response.text = `На ${forceToday ? 'сегодня' : 'завтра'} никто не записался, можете отдохнуть`;
-            response.response.end_session = true;
-        }
-    }
-
-    //Only for second step
-    if (step === 2) {
-    }
-
-    res.send(response)
-})
 app.post('/today', async (req, res) => {
     const response = getDefaultResponse(req.body);
 
