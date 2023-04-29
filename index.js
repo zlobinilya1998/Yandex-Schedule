8100c63f0c6d9de00c34b3d27a3c833318b574a9
qwertin1998@gmail.com <herejutep1998>
2023-04-30 02:42:15 +0300

    --

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const port = 3000;
 const app = express();
 
 
-app.get('/hi', (req,res) => {
-    res.send({
-        message: 'Hello my friend',
-    })
-})
+
 app.use(express.json())
 app.use(logger);
 app.use(router);
